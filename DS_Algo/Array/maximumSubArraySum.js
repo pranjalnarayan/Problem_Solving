51c171d2b7317fc69081eafeeed2492929334e4c
Kirty Kumari <pnarayan733@gmail.com>
2019-07-04 21:44:50 +0530

    Maximum sub array sum

---
+++ DS_Algo/Array/maximumSubArraySum.js
@@ -0,0 +1,15 @@
+
+const max = (num1, num2) => (num1 > num2 ? num1 : num2);
+
+const maxSubArray = arr => {
+	let maxSum = arr[0];
+	let curSum = arr[0];
+
+	for(let i = 1, len = arr.length; i < len; i++) {
+		let curNum = arr[i];
+        curSum = max(curNum, (curNum + curSum));
+		curSum > maxSum && (maxSum = curSum);
+    }
+
+	return maxSum;
+}
